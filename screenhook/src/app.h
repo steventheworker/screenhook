ee5d77a9104b17ca2e94c5107f480176d74abb2d
steventheworker <steventheworker@gmail.com>
2023-04-09 18:58:16 -0700

    refactor

--- screenhook/src/app.h
+++ screenhook/src/app.h
@@ -9,10 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 @interface app : NSObject
-+ (BOOL) contextMenuExists: (CGPoint)carbonPoint : (NSDictionary*)info;
 + (void) init;
-+ (void) focusDock;
-+ (float) maxDelay;
 + (BOOL) isSpotlightOpen : (BOOL) isAlfred;
 @end
 NS_ASSUME_NONNULL_END
--- screenhook/src/app.m
+++ screenhook/src/app.m
@@ -9,14 +9,6 @@
 #import "helperLib.h"
 
 //config
-const NSString* versionLink = @"https://dockalttab.netlify.app/currentversion.txt";
-const float TICK_DELAY = 0.16666665; // 0.33333 / 2   seconds
-const float DELAY_MAX = 2; // seconds
-
-//define
-const int CONTEXTDISTANCE = 150; //dock testPoint/contextmenu's approx. distance from pointer
-const int DOCK_OFFSET = 5; //5 pixels
-
 void askForAccessibility(void) {
     NSDictionary* options = @{(__bridge NSString*)(kAXTrustedCheckOptionPrompt) : @YES};
     if (!AXIsProcessTrustedWithOptions((__bridge CFDictionaryRef)options)) {
@@ -62,32 +54,6 @@ void askForAccessibility(void) {
     // init UI
     if (del->runningApps[@"BTT"]) [[del->BTTState cell] setTitle:@"Checking if afterBTTLaunched..."];
 }
-
-/* UI */
-/* utilities that depend on (AppDelegate *) */
-+ (BOOL) contextMenuExists:(CGPoint) carbonPoint : (NSDictionary*) info {
-    AppDelegate* del = [helperLib getApp];
-    if ([info[@"role"] isEqual:@"AXMenuItem"] || [info[@"role"] isEqual:@"AXMenu"]) return YES;
-    int multiplierX = [del->dockPos isEqual:@"left"] || [del->dockPos isEqual:@"right"] ? ([del->dockPos isEqual:@"left"] ? 1 : -1) : 0;
-    int multiplierY = [del->dockPos isEqual:@"bottom"] ? -1 : 0;
-    CGPoint testPoint = CGPointMake(carbonPoint.x + multiplierX * CONTEXTDISTANCE, carbonPoint.y + multiplierY * CONTEXTDISTANCE); //check if there is an open AXMenu @ testPoint next to the mouseLocation (DockLeft +x, DockRight -x, DockBottom -y)
-    NSDictionary* testInfo = [helperLib axInfo:[helperLib elementAtPoint:testPoint]];
-//    CFRelease(testPoint);
-    if ([testInfo[@"role"] isEqual:@"AXMenuItem"] || [testInfo[@"role"] isEqual:@"AXMenu"]) return YES;
-    return NO;
-}
-+ (void) focusDock {
-    NSLog(@"focusDock");
-    NSString* scriptTxt = @"tell application \"System Events\"\n\
-        key down 63\n\
-    delay 0.333\n\
-        key code 0\n\
-        key up 63\n\
-    end tell";
-    [helperLib runScript: scriptTxt];
-}
-+ (float) maxDelay {return DELAY_MAX;}
-+ (NSString*) getCurrentVersion {return @"" /*[helperLib fetch: (NSString*) versionLink] */ ;}
 + (BOOL) isSpotlightOpen : (BOOL) isAlfred {
     return ![[helperLib runScript: [NSString stringWithFormat: @"tell application \"System Events\" to tell process \"%@\" to count of windows", isAlfred ? @"Alfred" : @"Spotlight"]] isEqual:@"0"];
 }
