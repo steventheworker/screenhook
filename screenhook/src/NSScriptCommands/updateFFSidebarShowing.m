456fb85938fc9a7afa3d51f1a9f72691989f69a0
steventheworker <steventheworker@gmail.com>
2023-10-24 00:22:12 -0700

    add sdef, applescripts & NSScriptCommands folders

---
+++ screenhook/src/NSScriptCommands/updateFFSidebarShowing.m
@@ -0,0 +1,24 @@
+//
+//  updateFFSidebarShowing.m
+//  screenhook
+//
+//  Created by Steven G on 4/11/23.
+//
+
+#import "updateFFSidebarShowing.h"
+//#import "timer.h"
+
+@implementation updateFFSidebarShowing
+- (id) performDefaultImplementation {
+    NSDictionary* args = [self evaluatedArguments];
+    BOOL val = nil;
+    if (args.count) {
+        val = [[args valueForKey:@""] boolValue];    // get the direct argument
+    } else {
+        [self setScriptErrorNumber:-50]; // raise error
+        [self setScriptErrorString:@"Parameter Error: A Parameter is expected for the verb 'updateFFSidebarShowing' (You have to specify if the sidebar is showing! (hint: boolean))."];
+    }
+//    [timer updateFFSidebarShowing: val];
+    return nil;
+}
+@end
