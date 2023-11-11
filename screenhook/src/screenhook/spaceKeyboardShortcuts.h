51a1ae1f09822c18195d3bcb5ac7015efcdb3ca1
steventheworker <steventheworker@gmail.com>
2023-11-11 02:39:48 -0800

    space switch (1-9 hotkeys) keyboard fallback only

--- screenhook/src/screenhook/spaceKeyboardShortcuts.h
+++ screenhook/src/screenhook/spaceKeyboardShortcuts.h
@@ -10,7 +10,9 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface spaceKeyboardShortcuts : NSObject
++ (void) init;
 + (void) keyCode: (int) keyCode;
++ (void) spaceChanged: (NSNotification*) note;
 @end
 
 NS_ASSUME_NONNULL_END
--- screenhook/src/screenhook/spaceKeyboardShortcuts.m
+++ screenhook/src/screenhook/spaceKeyboardShortcuts.m
@@ -6,10 +6,24 @@
 //
 
 #import "spaceKeyboardShortcuts.h"
+#import "../globals.h"
+#import "../helperLib.h"
 #import "../Spaces.h"
+#import "../WindowManager.h"
 
 extern void CGSManagedDisplaySetCurrentSpace(const /* CGSConnectionID */ int cid, /* CGSManagedDisplay */ CFStringRef display, /* CGSSpace */ uint64_t space);
 extern /* CGSManagedDisplay */ CFStringRef kCGSPackagesMainDisplayIdentifier;
+
+void fallbackToKeys(int from, int to) {
+    int dx = to - from;
+    for (int i = 0; i < abs(dx); i++) {
+        NSString* scptStr = [NSString stringWithFormat: @"tell application \"System Events\" to key code %d using {control down}", dx < 0 ? 123 : 124];
+        setTimeout(^{
+            [helperLib applescript: scptStr];
+        }, ([WindowManager exposeType] ? 750 : 100) * i + 100);
+    }
+}
+
 @implementation spaceKeyboardShortcuts
 + (void) init {
     //onlaunch put invisible window on space
@@ -34,9 +48,7 @@ extern /* CGSManagedDisplay */ CFStringRef kCGSPackagesMainDisplayIdentifier;
     
     //if space does not yet have invisible window to activate, see if window on space exists you have axref for and activate that
         //else fallback to trigger keyboard shortcuts (control+leftarrow/rightarrow)
-    
-    //i thought this would let me go directly to space, but it just brings target space's windows into current space in a glitchy way
-    CGSManagedDisplaySetCurrentSpace([Spaces CGSMainConnectID], kCGSPackagesMainDisplayIdentifier, (uint64_t) [spaces[targetSpaceIndex][0] intValue]);
+    fallbackToKeys([Spaces currentSpaceIndex] - 1, targetSpaceIndex); //currentSpaceIndex starts at 1 instead of 0
 }
 + (void) spaceChanged: (NSNotification*) note {
     //onspacechange put invisible window on space (if DNE)
