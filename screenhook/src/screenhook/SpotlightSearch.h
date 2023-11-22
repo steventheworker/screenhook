f15af3416248db9062f5b4f07bb82e12667a1da4
steventheworker <steventheworker@gmail.com>
2023-11-22 05:39:11 -0800

    handle dock gap, live updating of dockpos and dockautohide, spotlight search works (live updating is accurate, but for some reason, can't update dockPos variable TRY INSTANCE OF CLASS)

---
+++ screenhook/src/screenhook/SpotlightSearch.h
@@ -0,0 +1,16 @@
+//
+//  SpotlightSearch.h
+//  screenhook
+//
+//  Created by Steven G on 11/22/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SpotlightSearch : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ screenhook/src/screenhook/SpotlightSearch.m
@@ -0,0 +1,12 @@
+//
+//  SpotlightSearch.m
+//  screenhook
+//
+//  Created by Steven G on 11/22/23.
+//
+
+#import "SpotlightSearch.h"
+
+@implementation SpotlightSearch
+
+@end
