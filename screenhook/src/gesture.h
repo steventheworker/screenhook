281f443db41bba00d32f7ef023c9a89f88dbe936
steventheworker <steventheworker@gmail.com>
2023-11-22 22:13:04 -0800

    bring back gesture manager

---
+++ screenhook/src/gesture.h
@@ -0,0 +1,16 @@
+//
+//  gesture.h
+//  screenhook
+//
+//  Created by Steven G on 11/22/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface gesture : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ screenhook/src/gesture.m
@@ -0,0 +1,12 @@
+//
+//  gesture.m
+//  screenhook
+//
+//  Created by Steven G on 11/22/23.
+//
+
+#import "gesture.h"
+
+@implementation gesture
+
+@end
