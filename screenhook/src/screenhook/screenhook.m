5893ab03fd3dbe6bec4c18dc735d18723af912b3
steventheworker <steventheworker@gmail.com>
2023-11-29 19:09:51 -0800

    make ARROWREPEAT_T longer-er

--- screenhook/src/screenhook/screenhook.m
+++ screenhook/src/screenhook/screenhook.m
@@ -32,7 +32,7 @@ BOOL dockAutohide = NO;
 AXUIElementRef dockContextMenuClickee; //the dock separator element that was right clicked
 
 //probably move into it's own ft. file
-const float ARROWREPEAT_T = 0.66; //seconds
+const float ARROWREPEAT_T = 0.666 * 2; //seconds
 NSDate* lastArrowExecT;
 
 @implementation screenhook
