0dcb5d063acd7fe47142f7f809d8e81b43f31eb9
steventheworker <steventheworker@gmail.com>
2023-12-07 04:01:13 -0800

    reformat lazyControlArrows

--- screenhook/src/screenhook/lazyControlArrows.m
+++ screenhook/src/screenhook/lazyControlArrows.m
@@ -19,49 +19,34 @@ NSDate* lastArrowSentT;
     lastArrowExecT = [NSDate date];
     lastArrowSentT = [NSDate date];
 }
++ (BOOL) shortcutUp {
+    NSDate* t0 = lastArrowExecT;
+    lastArrowExecT = NSDate.date;
+    if ([lastArrowExecT timeIntervalSinceDate: t0] <= ARROWREPEAT_T) {
+        return YES;
+    }
+    return NO;
+}
++ (BOOL) shortcutDown: (int) keyCode {
+    NSDate* t0 = lastArrowExecT;
+    lastArrowExecT = NSDate.date;
+    if ([lastArrowExecT timeIntervalSinceDate: t0] <= ARROWREPEAT_T) {
+        if ([lastArrowExecT timeIntervalSinceDate: lastArrowSentT] >= ARROWSEND_T) {
+            [helperLib sendKey: keyCode];
+            lastArrowSentT = lastArrowExecT;
+        }
+        return YES;
+    }
+    keyCode == 123 ? [spaceKeyboardShortcuts prevSpace] : [spaceKeyboardShortcuts nextSpace];
+    return NO;
+}
 + (BOOL) keyCode: (int) keyCode : (NSString*) eventString : (NSDictionary*) modifiers {
     //ctrl+left-arrow
-    if ([eventString isEqual: @"keydown"] && (modifiers[@"ctrl"]) && modifiers.count == 1 && keyCode == 123) {
-        NSDate* t0 = lastArrowExecT;
-        lastArrowExecT = NSDate.date;
-        if ([lastArrowExecT timeIntervalSinceDate: t0] <= ARROWREPEAT_T) {
-            if ([lastArrowExecT timeIntervalSinceDate: lastArrowSentT] >= ARROWSEND_T) {
-                [helperLib sendKey: 123];
-                lastArrowSentT = lastArrowExecT;
-            }
-            return YES;
-        }
-        [spaceKeyboardShortcuts prevSpace];
-        return NO;
-    }
-    if ([eventString isEqual: @"keyup"] && (modifiers[@"ctrl"]) && modifiers.count == 1 && keyCode == 123) {
-        NSDate* t0 = lastArrowExecT;
-        lastArrowExecT = NSDate.date;
-        if ([lastArrowExecT timeIntervalSinceDate: t0] <= ARROWREPEAT_T) {
-            return YES;
-        }
-        return NO;
-    }
+    if ([eventString isEqual: @"keydown"] && (modifiers[@"ctrl"]) && modifiers.count == 1 && keyCode == 123) return [self shortcutDown: keyCode];
+    if ([eventString isEqual: @"keyup"] && (modifiers[@"ctrl"]) && modifiers.count == 1 && keyCode == 123) return [self shortcutUp];
     //ctrl+right-arrow
-    if ([eventString isEqual: @"keydown"] && (modifiers[@"ctrl"]) && modifiers.count == 1 && keyCode == 124) {
-        NSDate* t0 = lastArrowExecT;
-        lastArrowExecT = NSDate.date;
-        if ([lastArrowExecT timeIntervalSinceDate: t0] <= ARROWREPEAT_T) {
-            if ([lastArrowExecT timeIntervalSinceDate: lastArrowSentT] >= ARROWSEND_T) {
-                [helperLib sendKey: 124];
-                lastArrowSentT = lastArrowExecT;
-            }
-            return YES;
-        }
-        [spaceKeyboardShortcuts nextSpace];
-        return NO;
-    }
-    if ([eventString isEqual: @"keyup"] && (modifiers[@"ctrl"]) && modifiers.count == 1 && keyCode == 124) {
-        NSDate* t0 = lastArrowExecT;
-        lastArrowExecT = NSDate.date;
-        if ([lastArrowExecT timeIntervalSinceDate: t0] <= ARROWREPEAT_T) return YES;
-        return NO;
-    }
+    if ([eventString isEqual: @"keydown"] && (modifiers[@"ctrl"]) && modifiers.count == 1 && keyCode == 124) return [self shortcutDown: keyCode];;
+    if ([eventString isEqual: @"keyup"] && (modifiers[@"ctrl"]) && modifiers.count == 1 && keyCode == 124) return [self shortcutUp];
     return YES;
 }
 @end
